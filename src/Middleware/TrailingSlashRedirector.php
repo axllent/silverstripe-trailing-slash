cd1e5465955f5bd96b428481777193435a1ad9e0
Ralph Slooten <axllent@gmail.com>
2019-12-11 15:26:29 +1300

    PSR2 updates

--- src/Middleware/TrailingSlashRedirector.php
+++ src/Middleware/TrailingSlashRedirector.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Axllent\TrailingSlash\Middleware;
 
 use SilverStripe\Control\Controller;
@@ -18,6 +17,7 @@ class TrailingSlashRedirector implements HTTPMiddleware
 {
     /**
      * URLS to ignore
+     *
      * @var array
      */
     private static $ignore_paths = [
@@ -25,10 +25,21 @@ class TrailingSlashRedirector implements HTTPMiddleware
         'dev/',
     ];
 
+    /**
+     * Process request
+     *
+     * @param HTTPRequest $request  HTTP request
+     * @param callable    $delegate Delegate
+     *
+     * @return HTTPResponse
+     */
     public function process(HTTPRequest $request, callable $delegate)
     {
         $ignore_paths = [];
-        $ignore_config = Config::inst()->get(TrailingSlashRedirector::class, 'ignore_paths');
+
+        $ignore_config = Config::inst()
+            ->get(TrailingSlashRedirector::class, 'ignore_paths');
+
         foreach ($ignore_config as $iurl) {
             if ($quoted = preg_quote(ltrim($iurl, '/'), '/')) {
                 array_push($ignore_paths, $quoted);
@@ -37,25 +48,36 @@ class TrailingSlashRedirector implements HTTPMiddleware
 
         if ($request && ($request->isGET() || $request->isHEAD())) {
             // skip $ignore_paths and home (`/`)
-            if ($request->getURL() == '' ||
-                preg_match('/^(' . implode('|',$ignore_paths) . ')/i', $request->getURL())
+            if ($request->getURL() == ''
+                || preg_match(
+                    '/^(' . implode('|', $ignore_paths) . ')/i',
+                    $request->getURL()
+                )
             ) {
                 return $delegate($request);
             }
 
             $requested_url = $_SERVER['REQUEST_URI'];
-            $expected_url = rtrim(Director::baseURL() . $request->getURL(), '/') . '/';
+
+            $expected_url = rtrim(
+                Director::baseURL() . $request->getURL(), '/'
+            ) . '/';
+
             $urlPathInfo = pathinfo($requested_url);
+
             $params = $request->getVars();
 
-            if (!Director::is_ajax() &&
-                !isset($urlPathInfo['extension']) &&
-                empty($params) &&
-                !preg_match('/^' . preg_quote($expected_url, '/') . '(?!\/)/i', $requested_url)
+            if (!Director::is_ajax()
+                && !isset($urlPathInfo['extension'])
+                && empty($params)
+                && !preg_match(
+                    '/^' . preg_quote($expected_url, '/') . '(?!\/)/i',
+                    $requested_url
+                )
             ) {
-                $params = $request->getVars();
+                $params       = $request->getVars();
                 $redirect_url = Controller::join_links($expected_url, '/');
-                $response = new HTTPResponse();
+                $response     = new HTTPResponse();
 
                 return $response->redirect($redirect_url, 301);
             }
